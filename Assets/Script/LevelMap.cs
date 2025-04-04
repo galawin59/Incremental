852ffced761196ec31d55ba29d95835f3db4bc67
cadzuar <half.craft36@gmail.com>
2025-04-04 20:31:33 +0200

    combat scene

---
+++ Assets/Script/LevelMap.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class LevelMap : MonoBehaviour
+{
+    Room startRoom;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Room
+{
+    Room up;
+    Room down;
+    Room left;
+    Room right;
+
+
+
+}
